a4a92500a2adc81b1dfb8a06ca40fbcb71bc71e6
zsolt <kalman.zsolt@sparkconsulting.hu>
2020-12-28 10:28:35 +0100

    Add boilerpate

---
+++ src/Application/Car/Commands/CreateCar/CreateCarCommand.cs
@@ -0,0 +1,32 @@
+﻿using ExampleProject.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Application.Car.Commands
+{
+    public class CreateCarCommand : IRequest<int>
+    {
+        public string Car { get; set; }
+    }
+
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        public CreateCarCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            var car = new Car
+            {
+                
+            }
+        }
+    }
+}
