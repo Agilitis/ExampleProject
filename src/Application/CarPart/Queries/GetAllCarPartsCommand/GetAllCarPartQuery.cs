7afe51e3d65575073f0c20f54e14a9a240179b6b
zsolt <kalman.zsolt@sparkconsulting.hu>
2020-12-30 10:24:27 +0100

    Add rent commands and handlers

--- src/Application/CarPart/Queries/GetAllCarPartsCommand/GetAllCarPartQuery.cs
+++ src/Application/CarPart/Queries/GetAllCarPartsCommand/GetAllCarPartQuery.cs
@@ -1,7 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ExampleProject.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 namespace ExampleProject.Application.CarPart.Queries.GetAllCarPartsCommand
 {
-    public class GetAllCarPartQuery
+    public class GetAllCarPartQuery : IRequest<IEnumerable<Domain.Entities.CarPart>>
     {
         
+    }
+    
+    public class GetAllCarPartQueryHandler : IRequestHandler<GetAllCarPartQuery, IEnumerable<Domain.Entities.CarPart>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllCarPartQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.CarPart>> Handle(GetAllCarPartQuery request, CancellationToken cancellationToken)
+        {
+            return _context.CarParts
+                .AsNoTracking();
+        }
     }
 }
--- src/Application/Rent/Commands/CreateRent/CreateRentCommand.cs
+++ src/Application/Rent/Commands/CreateRent/CreateRentCommand.cs
@@ -1,7 +1,42 @@
-namespace ExampleProject.Application.Rent.Commands.CreateRentCommand
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ExampleProject.Application.Common.Interfaces;
+using MediatR;
+
+namespace ExampleProject.Application.Rent.Commands.CreateRent
 {
-    public class CreateRentCommand
+    public class CreateRentCommand : IRequest<int>
     {
-        
+        public int CarId { get; set; }
+        public int RentLengthInDays { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+    
+    public class CreateRentCommandHandler : IRequestHandler<CreateRentCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateRentCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateRentCommand request, CancellationToken cancellationToken)
+        {
+            var car = await _context.Cars.FindAsync(request.CarId);
+            var rent = new Domain.Entities.Rent
+            {
+                Car = car,
+                RentLength = TimeSpan.FromDays(request.RentLengthInDays),
+                StartDate = request.StartDate
+            };
+
+            _context.Rents.Add(rent);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return rent.Id;
+        }
     }
 }
